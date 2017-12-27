1a299e5a9f378d46790f8075573819a99b7c6cb2
Tiago Santos <tiagossilva_95@hotmail.com>
2017-12-27 00:11:25 +0000

    Inset Text Field.
    Created a custom UITextField class. This class type is set as the custom class of the email and password text fields of the Login View Controller.

--- Breakpoint/Views/InsetTextField.swift
+++ Breakpoint/Views/InsetTextField.swift
@@ -13,15 +13,25 @@ class InsetTextField: UITextField {
     private let textRectOffSet: CGFloat = 20
     private let padding: UIEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
     
-//    override func textRect(forBounds bounds: CGRect) -> CGRect {
-//        <#code#>
-//    }
-//
-//    override func editingRect(forBounds bounds: CGRect) -> CGRect {
-//        <#code#>
-//    }
-//
-//    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
-//        <#code#>
-//    }
+    override func awakeFromNib() {
+        setupView()
+        super.awakeFromNib()
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    func setupView() {
+        let placeHolder = NSAttributedString(string: self.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        self.attributedPlaceholder = placeHolder
+    }
 }
