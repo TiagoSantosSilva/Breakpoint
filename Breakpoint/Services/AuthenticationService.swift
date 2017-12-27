6f6afd132e364371f8d514fec7e24fad9f370a9e
Tiago Santos <tiagossilva_95@hotmail.com>
2017-12-27 11:00:39 +0000

    Configuration of the Authentication Service.

---
+++ Breakpoint/Services/AuthenticationService.swift
@@ -0,0 +1,43 @@
+//
+//  AuthenticationService.swift
+//  Breakpoint
+//
+//  Created by Tiago Santos on 27/12/17.
+//  Copyright © 2017 Tiago Santos. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthenticationService {
+    
+    let dataService: DataService!
+    
+    init(dataService: DataService) {
+        self.dataService = dataService
+    }
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID, "email": user.email]
+            
+            self.dataService.createDatabaseUser(uniqueId: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard user != nil else {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+}
