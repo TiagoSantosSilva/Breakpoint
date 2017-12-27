526b209a6bebe7c2df68a98c187ee0cc2d1a5864
Tiago Santos <tiagossilva_95@hotmail.com>
2017-12-27 00:24:05 +0000

    Added a shadow to the grey view in the Authentication View Controller.

---
+++ Breakpoint/Cocoa Touches/ShadowView.swift
@@ -0,0 +1,23 @@
+//
+//  ShadowView.swift
+//  Breakpoint
+//
+//  Created by Tiago Santos on 27/12/17.
+//  Copyright © 2017 Tiago Santos. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        setupView()
+        super.awakeFromNib()
+    }
+    
+    func setupView() {
+        self.layer.shadowOpacity = 0.75
+        self.layer.shadowRadius = 5
+        self.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+}
