bf61bab5225feba1f41a6d26efe403374ec9604e
Tiago Santos <tiagossilva_95@hotmail.com>
2017-12-27 16:30:23 +0000

    Created a custom class for the Feed Table View Cell.

---
+++ Breakpoint/Cocoa Touches/FeedTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  FeedTableViewCell.swift
+//  Breakpoint
+//
+//  Created by Tiago Santos on 27/12/17.
+//  Copyright © 2017 Tiago Santos. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLabel.text = email
+        self.messageLabel.text = content
+    }
+}
